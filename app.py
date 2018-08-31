6cbe82b0225db91c13817c6c3550134049be7884
AiryShift <religionofaperturelabs@gmail.com>
2018-08-31 17:45:59 +1000

    Try subprocess?

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import time
 
 from flask import Flask, redirect, render_template, send_from_directory, url_for
@@ -20,9 +21,9 @@ def cv():
         original_wd = os.getcwd()
         try:
             os.chdir(os.path.join(os.getcwd(), 'cv'))
-            os.system('git pull')
-            os.system('pdflatex cv.tex')
-            os.system('mv cv.pdf ../static/')
+            subprocess.run('git pull', shell=True)
+            subprocess.run('pdflatex cv.tex', shell=True)
+            subprocess.run('mv cv.pdf ../static/', shell=True)
         finally:
             os.chdir(original_wd)
     return redirect(url_for('static', filename='cv.pdf'))
